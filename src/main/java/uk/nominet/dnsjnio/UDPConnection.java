e87fa061ea454f9a69aa2944a6917c3142371370
John Yeary <jyeary@bluelotussoftware.com>
2016-05-20 09:24:31 -0400

    1. Formatted for clarity. 2. Updated Javadocs. 3. Fixed imports.

--- src/main/java/uk/nominet/dnsjnio/UDPConnection.java
+++ src/main/java/uk/nominet/dnsjnio/UDPConnection.java
@@ -13,19 +13,17 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and 
 limitations under the License.
  */
-
 package uk.nominet.dnsjnio;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.nio.channels.SelectionKey;
-
 import org.xbill.DNS.Message;
+import static uk.nominet.dnsjnio.NonblockingResolver.getNewInetSocketAddressWithRandomPort;
 
 /**
- * This class implements the UDP specific methods for the
- * Connection superclass.
+ * This class implements the UDP specific methods for the Connection superclass.
  */
 public class UDPConnection extends Connection {
 
@@ -33,72 +31,77 @@ public class UDPConnection extends Connection {
         super(listener, udpSize);
     }
 
+    @Override
     protected void connect() {
         try {
             DatagramChannel sch = DatagramChannel.open();
             sch.configureBlocking(false);
-            
+
             // Pick up SocketException here, and keep rebinding to different random port
             boolean connectedOk = false;
             while (!connectedOk) {
-              try {
-        	    sch.socket().bind(localAddress);
-        	    connectedOk = true;
-              } catch (java.net.SocketException e) {
-            	// Failure may be caused by picking a port number that was
-            	// already in use. Pick another random port and try again.
-            	// Note that the socket channel is now invalid, we need to
-            	// close it and open a fresh one.
-            	localAddress = uk.nominet.dnsjnio.NonblockingResolver.getNewInetSocketAddressWithRandomPort(localAddress.getAddress());
-            	sch.close();
-            	sch = DatagramChannel.open();
-            	sch.configureBlocking(false);
-              }
+                try {
+                    sch.socket().bind(localAddress);
+                    connectedOk = true;
+                } catch (java.net.SocketException e) {
+                    // Failure may be caused by picking a port number that was
+                    // already in use. Pick another random port and try again.
+                    // Note that the socket channel is now invalid, we need to
+                    // close it and open a fresh one.
+                    localAddress = getNewInetSocketAddressWithRandomPort(localAddress.getAddress());
+                    sch.close();
+                    sch = DatagramChannel.open();
+                    sch.configureBlocking(false);
+                }
             }
-            sk = sch.register(DnsController.getSelector(),0);
+            sk = sch.register(DnsController.getSelector(), 0);
             sch.connect(remoteAddress);
             attach(sk);
-        } catch(Exception e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
             close();
         }
     }
 
-
     /**
-     * attach key and channel and set connection interest in selection key
+     * Attach key and channel and set connection interest in selection key
+     *
+     * @param sk The {@link SelectionKey} to attach.
      */
     public void attach(SelectionKey sk) {
         this.sk = sk;
         sk.attach(this);
         DatagramChannel sch = (DatagramChannel) sk.channel();
-        if(sch.isConnected()) {
+        if (sch.isConnected()) {
             sk.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
             setState(State.OPENED);
         }
     }
 
     /**
-     * process a connect complete selection
+     * <p>
+     * Process a connect complete selection.</p>
+     * <p>
+     * <strong>Note:</strong> This is a NO-OP. Nothing to do for UDP</p>
      */
-    public void doConnect()	{
-        // Nothing to do for UDP
+    @Override
+    public void doConnect() {
+        //NO-OP 
     }
 
     /**
-     * process a read ready selection
+     * Process a read ready selection
      */
+    @Override
     public void doRead() {
-    	
-    	// Make sure that the IP we're receiving from is the IP we sent to!
-    	// This is done by the DatagramChannel, which only receives datagrams
-    	// from the peer it is connected with.
-    	
-        DatagramChannel sc = (DatagramChannel)sk.channel();
+
+        // Make sure that the IP we're receiving from is the IP we sent to!
+        // This is done by the DatagramChannel, which only receives datagrams
+        // from the peer it is connected with.
+        DatagramChannel sc = (DatagramChannel) sk.channel();
         try {
             readFromChannel(sc);
-        }
-        catch (NullPointerException e) {
+        } catch (NullPointerException e) {
             return;
         }
 
@@ -109,7 +112,7 @@ public class UDPConnection extends Connection {
                 byte[] packet = new byte[recvCount];
                 System.arraycopy(recvBytes, 0, packet, 0, recvCount);
                 try {
-                	// Get the first packet in the buffer
+                    // Get the first packet in the buffer
                     Message m = new Message(packet);
                     if (m.numBytes() < recvCount) {
                         packet = new byte[m.numBytes()];
@@ -126,15 +129,18 @@ public class UDPConnection extends Connection {
     }
 
     /**
-     * write out a byte buffer
+     * Write out a byte buffer
+     *
+     * @param data The data to write to the socket.
      */
+    @Override
     protected void write(ByteBuffer data) {
-        DatagramChannel sc = (DatagramChannel)sk.channel();
-        if(sc.isOpen()) {
-            if(data.hasRemaining()) {
+        DatagramChannel sc = (DatagramChannel) sk.channel();
+        if (sc.isOpen()) {
+            if (data.hasRemaining()) {
                 try {
                     sc.write(data);
-                } catch(IOException e) {
+                } catch (IOException e) {
                     closeComplete();
                 }
             }
@@ -143,39 +149,42 @@ public class UDPConnection extends Connection {
     }
 
     /**
-     * close the connection and its socket
+     * Close the connection and its socket
+     *
+     * @return {@code true} if the socket was successfully closed, and
+     * {@code false} otherwise.
      */
+    @Override
     protected boolean close() {
-    	boolean didClose = false;
-        if((getState() != State.CLOSED) && (getState() != State.CLOSING)) {
-        	// Fix for 20080801 bug reported by :
-        	// Allan O'Driscoll for sporadic NullPointerException - thanks, Allan!
-        	if (sk != null) {
-                DatagramChannel sc = (DatagramChannel)sk.channel();
-                if(sc != null && sc.isOpen()) {
+        boolean didClose = false;
+        if ((getState() != State.CLOSED) && (getState() != State.CLOSING)) {
+            // Fix for 20080801 bug reported by :
+            // Allan O'Driscoll for sporadic NullPointerException - thanks, Allan!
+            if (sk != null) {
+                DatagramChannel sc = (DatagramChannel) sk.channel();
+                if (sc != null && sc.isOpen()) {
                     didClose = true;
-                    if(getState() == State.OPENED) {
+                    if (getState() == State.OPENED) {
                         sk.interestOps(0);
                         setState(State.CLOSING);
                         try {
                             sc.close();
-                        }
-                        catch (IOException e) {
+                        } catch (IOException e) {
                             e.printStackTrace();
-                        //     log error
+                            //     log error
                         }
                     }
                     closeComplete();
                 }
-        	}
+            }
         }
         return didClose;
-//        return true;
     }
 
+    @Override
     protected void closeChannel() throws IOException {
-        DatagramChannel sc = (DatagramChannel)sk.channel();
-        if(sc != null && sc.isOpen()) {
+        DatagramChannel sc = (DatagramChannel) sk.channel();
+        if (sc != null && sc.isOpen()) {
             sc.close();
         }
     }
