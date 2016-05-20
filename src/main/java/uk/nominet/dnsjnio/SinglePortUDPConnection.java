4f537128d0cc9d5e0588936ed80b785f683bd90b
John Yeary <jyeary@bluelotussoftware.com>
2016-05-20 09:16:34 -0400

    Formatted for readability, and set the stacktrace to print to System.err.

--- src/main/java/uk/nominet/dnsjnio/SinglePortUDPConnection.java
+++ src/main/java/uk/nominet/dnsjnio/SinglePortUDPConnection.java
@@ -13,30 +13,31 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and 
 limitations under the License.
  */
-
 package uk.nominet.dnsjnio;
 
 import java.nio.channels.DatagramChannel;
 
 /**
- * Single port UDP connection.
- * This class reuses the same port.
+ * Single port UDP connection. This class reuses the same port.
  */
 public class SinglePortUDPConnection extends UDPConnection {
+
     public SinglePortUDPConnection(ConnectionListener listener, int port) {
         super(listener, SINGLE_PORT_BUFFER_SIZE);
     }
+
+    @Override
     protected void connect() {
         try {
             DatagramChannel sch = DatagramChannel.open();
             sch.configureBlocking(false);
             sch.socket().setReuseAddress(true);
-        	sch.socket().bind(localAddress);
-            sk = sch.register(DnsController.getSelector(),0);
+            sch.socket().bind(localAddress);
+            sk = sch.register(DnsController.getSelector(), 0);
             sch.connect(remoteAddress);
             attach(sk);
-        } catch(Exception e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
             close();
         }
     }
