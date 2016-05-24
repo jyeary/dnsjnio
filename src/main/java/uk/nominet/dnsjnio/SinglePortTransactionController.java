4948538c828624ff49a3fb46ab22df29678d7bfb
John Yeary <jyeary@bluelotussoftware.com>
2016-05-24 13:16:12 -0400

    Formatted for clarity.

--- src/main/java/uk/nominet/dnsjnio/SinglePortTransactionController.java
+++ src/main/java/uk/nominet/dnsjnio/SinglePortTransactionController.java
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and 
 limitations under the License.
  */
-
 package uk.nominet.dnsjnio;
 
 import java.io.IOException;
@@ -26,25 +25,27 @@ import org.xbill.DNS.Message;
 import org.xbill.DNS.ResolverListener;
 
 /**
- * This class provides communication over a single port.
- * Each Resolver will run on a different port, which has one TCP and one UDP connection to a server.
- * When a query comes in, we need to check status of current Connection
- * Either reuse it or reopen it.
- * If a query needs to sent with a header ID which is currently in use on this port, then a new standard Transaction object is used on a new port.
- * When a query ends (response or timeout) then the numQueries should be decremented and the Connection closed if numQueries == 0.
+ * This class provides communication over a single port. Each Resolver will run
+ * on a different port, which has one TCP and one UDP connection to a server.
+ * When a query comes in, we need to check status of current Connection Either
+ * reuse it or reopen it. If a query needs to sent with a header ID which is
+ * currently in use on this port, then a new standard Transaction object is used
+ * on a new port. When a query ends (response or timeout) then the numQueries
+ * should be decremented and the Connection closed if numQueries == 0.
  */
 public class SinglePortTransactionController extends AbstractTransaction {
+
     // Keep list of outstanding queries (connection, responseQueue, listener, id)
     // When a packet comes in, get the id, and check all outstanding queries for that id.
     private Map<Integer, QueryData> tcpQueryDataMap = new HashMap<>();
-    private Map<Integer,QueryData> udpQueryDataMap = new HashMap<>();
+    private Map<Integer, QueryData> udpQueryDataMap = new HashMap<>();
     private TCPConnection tcpConnection;
     private UDPConnection udpConnection;
     protected InetSocketAddress remoteAddress;
     protected InetSocketAddress localAddress;
     static int udpOpenedCount = 0;
     static int udpOpeningCount = 0;
-    
+
     public SinglePortTransactionController(InetSocketAddress remoteAddr, InetSocketAddress localAddr) {
         this.remoteAddress = remoteAddr;
         this.localAddress = localAddr;
@@ -55,15 +56,15 @@ public class SinglePortTransactionController extends AbstractTransaction {
             udpQueryDataMap = new HashMap();
         }
     }
-    
+
     public boolean headerIdNotInUse(int id) {
         // Search the queryDataList for this ID.
-        synchronized(tcpQueryDataMap) {
+        synchronized (tcpQueryDataMap) {
             if (tcpQueryDataMap.keySet().contains(new Integer(id))) {
                 return false;
             }
         }
-        synchronized(udpQueryDataMap) {
+        synchronized (udpQueryDataMap) {
             if (udpQueryDataMap.keySet().contains(new Integer(id))) {
                 return false;
             }
@@ -77,7 +78,7 @@ public class SinglePortTransactionController extends AbstractTransaction {
     protected void startConnect(QueryData qData) {
         startTimer(qData);
         if (qData.isTcp()) {
-            synchronized(tcpQueryDataMap) {
+            synchronized (tcpQueryDataMap) {
                 tcpQueryDataMap.put(new Integer(qData.getQuery().getHeader().getID()), qData);
             }
             if (tcpConnection != null) {
@@ -95,23 +96,19 @@ public class SinglePortTransactionController extends AbstractTransaction {
                 if (tcpConnection.getState() == Connection.State.OPENED) {
                     readyToSend(tcpConnection);
                     return;
-                }
-                else if (tcpConnection.getState() == Connection.State.CLOSING) {
+                } else if (tcpConnection.getState() == Connection.State.CLOSING) {
                     // The query is in the queue. When the socket is closed, it will reconnect
                     return;
-                }
-                else if (tcpConnection.getState() == Connection.State.CLOSED) {
+                } else if (tcpConnection.getState() == Connection.State.CLOSED) {
                     // Reopen the connection
                     getNewTcpConnection(qData);
                 }
-            }
-            else {
+            } else {
                 // Fire up a new tcpConnection
                 getNewTcpConnection(qData);
             }
-        }
-        else {
-            synchronized(udpQueryDataMap) {
+        } else {
+            synchronized (udpQueryDataMap) {
                 udpQueryDataMap.put(new Integer(qData.getQuery().getHeader().getID()), qData);
             }
             if (udpConnection != null && !(udpConnection.getState() == Connection.State.CLOSED)) {
@@ -126,13 +123,11 @@ public class SinglePortTransactionController extends AbstractTransaction {
                 if (udpConnection.getState() == Connection.State.OPENED) {
                     readyToSend(udpConnection);
                     return;
-                }
-                else if (udpConnection.getState() == Connection.State.CLOSING) {
+                } else if (udpConnection.getState() == Connection.State.CLOSING) {
                     // The query is in the queue. When the socket is closed, it will reconnect
                     return;
                 }
-            }
-            else {
+            } else {
                 getNewUdpConnection(qData);
             }
         }
@@ -157,10 +152,9 @@ public class SinglePortTransactionController extends AbstractTransaction {
         this.localAddress = addr;
     }
 
-  
-
     /**
      * Send a query. This kicks off the whole process.
+     *
      * @param qData
      * @param id
      * @param responseQueue
@@ -175,6 +169,7 @@ public class SinglePortTransactionController extends AbstractTransaction {
 
     /**
      * Send a query using a ResolverListener. This kicks off the whole process.
+     *
      * @param qData
      * @param id
      * @param listener
@@ -202,7 +197,7 @@ public class SinglePortTransactionController extends AbstractTransaction {
         // Remove this query from the list
         Map queryMap = getQueryDataMap(qData.getConnection());
         boolean disconnect = false;
-        synchronized(queryMap) {
+        synchronized (queryMap) {
             queryMap.remove(new Integer(qData.getQuery().getHeader().getID()));
             if (queryMap.size() == 0) {
                 disconnect = true;
@@ -215,8 +210,7 @@ public class SinglePortTransactionController extends AbstractTransaction {
     }
 
     /**
-     * Called to say that we are readyToSend.
-     * We can now send the data.
+     * Called to say that we are readyToSend. We can now send the data.
      */
     public void readyToSend(Connection connection) {
         QueryData qData = null;
@@ -226,8 +220,7 @@ public class SinglePortTransactionController extends AbstractTransaction {
                 qData.setSent(true);
                 sendQuery(connection, qData.getQuery());
             }
-        }
-        while (qData != null);
+        } while (qData != null);
     }
 
     private QueryData getNextQueryData(Connection c) {
@@ -235,7 +228,7 @@ public class SinglePortTransactionController extends AbstractTransaction {
         synchronized (queryMap) {
             // @todo Can we optimise this?
             for (Iterator it = queryMap.values().iterator(); it.hasNext();) {
-                QueryData qData = (QueryData)(it.next());
+                QueryData qData = (QueryData) (it.next());
                 if (!(qData.isSent()) && (qData.getConnection() == c)) {
                     return qData;
                 }
@@ -254,6 +247,7 @@ public class SinglePortTransactionController extends AbstractTransaction {
 
     /**
      * The Connection has been closed
+     *
      * @param connection
      */
     public void closed(Connection connection) {
@@ -261,7 +255,7 @@ public class SinglePortTransactionController extends AbstractTransaction {
         Map queryMap = getQueryDataMap(connection);
         boolean reconnect = false;
         synchronized (queryMap) {
-            if (queryMap.size()!=0) {
+            if (queryMap.size() != 0) {
                 reconnect = true;
             }
         }
@@ -272,9 +266,10 @@ public class SinglePortTransactionController extends AbstractTransaction {
     }
 
     /**
-     * A packet is available. Decode it and act accordingly.
-     * If the packet is truncated over UDP, and ignoreTruncation
-     * is false, then a tcp query is run to return the whole response.
+     * A packet is available. Decode it and act accordingly. If the packet is
+     * truncated over UDP, and ignoreTruncation is false, then a tcp query is
+     * run to return the whole response.
+     *
      * @param data
      */
     public void dataAvailable(byte[] data, Connection connection) {
@@ -287,19 +282,18 @@ public class SinglePortTransactionController extends AbstractTransaction {
 
             // Search the list for this connection
             Map queryMap = getQueryDataMap(connection);
-            synchronized(queryMap) {
-                qData = (QueryData)(queryMap.get(new Integer(message.getHeader().getID())));
+            synchronized (queryMap) {
+                qData = (QueryData) (queryMap.get(new Integer(message.getHeader().getID())));
             }
             if (qData == null) {
-                    return; // @todo !!!
+                return; // @todo !!!
             }
             disconnect(qData);
 
             NonblockingResolver.verifyTSIG(qData.getQuery(), message, data, qData.getTsig());
             // Now check that we got the whole message, if we're asked to do so
-            if (!qData.isTcp() && !qData.isIgnoreTruncation() &&
-                    message.getHeader().getFlag(Flags.TC))
-            {
+            if (!qData.isTcp() && !qData.isIgnoreTruncation()
+                    && message.getHeader().getFlag(Flags.TC)) {
                 cancelTimer(qData);
                 // Redo the query, but use tcp this time.
                 qData.setTcp(true);
@@ -309,47 +303,46 @@ public class SinglePortTransactionController extends AbstractTransaction {
             }
 //            System.out.println("Returning id = " + qData.getId() + ", header id " + qData.getQuery().getHeader().getID());
             returnResponse(message, qData);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             return; // Ignore it. Query will time out eventually.
         }
     }
 
     /**
      * Return the response to the listener
+     *
      * @param message the response
      */
     private void returnResponse(Message message, QueryData qData) {
-    	if (!qData.isAnswered()) {
-    		qData.setAnswered(true);
+        if (!qData.isAnswered()) {
+            qData.setAnswered(true);
             // Stop the timer!
             cancelTimer(qData);
             returnResponse(qData.getListener(), qData.getResponseQueue(), message, qData.getId());
-    	}
+        }
     }
 
     /**
      * Throw an Exception to the listener
      */
     protected void returnException(Exception e, QueryData qData) {
-    	if (!qData.isAnswered()) {
-    		qData.setAnswered(true);
+        if (!qData.isAnswered()) {
+            qData.setAnswered(true);
             // Stop the timer!
             cancelTimer(qData);
 //          System.out.println("Exception for " +qData.getQuery().getHeader().getID());
             returnException(qData.getListener(), qData.getResponseQueue(), e, qData.getId());
-    	}
+        }
     }
 
     /**
-     * Cancel the timeout callback
-     * Also removes the QueryData from the list
+     * Cancel the timeout callback Also removes the QueryData from the list
      */
     private void cancelTimer(QueryData qData) {
         Timer.cancelTimeout(this, qData);
-        qData.setResponded (true);
+        qData.setResponded(true);
         Map queryMap = getQueryDataMap(qData.getConnection());
-        synchronized(queryMap) {
+        synchronized (queryMap) {
             queryMap.remove(new Integer(qData.getQuery().getHeader().getID()));
         }
     }
